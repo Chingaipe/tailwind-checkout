cc992d0a3401c0efc076d50343d5b9e4b47871ac
Chingaipe <mac.alicks@gmail.com>
2020-08-07 17:49:59 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      borderRadius: {
+        'xl': '0.75rem',
+        '2xl': '1rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
